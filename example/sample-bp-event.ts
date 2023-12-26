0d1fcd95abd2c33603bc4a9079b24776a773d544
Joseph Lawson <joe@joekiller.com>
2023-12-26 17:41:37 -0500

    feat: sample event with session, increment, and timestamp

--- example/sample-bp-event.ts
+++ example/sample-bp-event.ts
@@ -1,6 +1,39 @@
 import WSManager from '../src/index';
 
-const manager = new WSManager<unknown[]>('wss://ws.backpack.tf/events');
+/**
+ * Fired whenever a listing is created or updated. The exception to this is whenever listings are bumped, or when currency values are updated from a price suggestion (this causes all listings to have their value recalculated).
+ *
+ * Re-publishing an archived listing is considered an update.
+ */
+type listingUpdate = 'listing-update';
+
+/**
+ * Fired whenever a listing is deleted. Moving a listing to the archive is also considered to be deleting the listing.
+ */
+type listingDelete = 'listing-delete';
+interface BPEvent {
+  /**
+   * Each event has a unique 12-byte alphanumeric ID. The first four bytes of the ID is a 32-bit timestamp. {@link https://www.mongodb.com/docs/manual/reference/method/ObjectId/ Check here} for more information on this format.
+   */
+  id: string;
+  event: listingUpdate | listingDelete;
+  payload: unknown;
+}
+
+const parseEventId = (eventId: string): { timestamp: number; timeDate: Date; session: number; increment: number } => {
+  const rawEventId = Buffer.from(eventId, 'hex');
+  const timestamp = rawEventId.readIntBE(0, 4);
+  const session = rawEventId.readIntLE(4, 5);
+  const increment = rawEventId.readIntBE(9, 3);
+  return {
+    timestamp,
+    timeDate: new Date(timestamp * 1000),
+    session,
+    increment,
+  };
+};
+
+const manager = new WSManager<BPEvent[]>('wss://ws.backpack.tf/events');
 manager.on('error', (err) => {
   console.error(err);
 });
@@ -10,7 +43,18 @@ const run = async () => {
   while (manager.length == 0) {
     await new Promise((resolve) => setTimeout(() => resolve(undefined), 100));
   }
-  console.log(JSON.stringify(manager.getMessages()[0][0], undefined, 2));
+  const events = manager.getMessages()[0];
+  const event = events[0];
+  console.log('Sample Event:');
+  console.log(JSON.stringify(event, undefined, 2));
+  for (const e of events) {
+    const parsed = parseEventId(e.id);
+    console.log(
+      `session: ${parsed.session} inc: ${parsed.increment} time: ${
+        parsed.timestamp
+      } date: ${parsed.timeDate.toString()}`,
+    );
+  }
   manager.shutdown();
 };
 
